13eb73905c1e1ccba683bdf0f5b318660fd23d1a
daniel <danieelsan1y@gmail.com>
2022-09-04 18:10:57 -0300

    alteração entrada de arma

---
+++ src/app/shared/dto/filtro-modelo-arma.dto.ts
@@ -0,0 +1,46 @@
+import { HttpParams } from '@angular/common/http';
+
+/**
+ * Classe de trânsferencia com os parâmetros utilizados em filtros de pesquisa de Tipo Amigo.
+ *
+ * @author Guiliano Rangel (UEG)
+ */
+export class FiltroModeloArmaDTO {
+
+  /**
+   * Construtor da classe.
+   *
+   * @param modelo
+   */
+  constructor(
+    public modelo?: string
+  ) { }
+
+  /**
+   * Transforma o JSON do parâmetro em um objeto do modelo de dominio da aplicação.
+   *
+   * @param jsonData
+   */
+  static fromJson(jsonData: any): FiltroModeloArmaDTO {
+    return Object.assign(new FiltroModeloArmaDTO(), jsonData);
+  }
+
+  /**
+   * Retorna uma nova instancia de FiltroDTO
+   */
+  static getInstace(): FiltroModeloArmaDTO {
+    return new FiltroModeloArmaDTO();
+  }
+
+  /**
+   * Retorna a instância de HttpParams considerando os parâmetros informados.
+   */
+  public toParams(): HttpParams {
+    let params = new HttpParams();
+
+    if (this.modelo) {
+      params = params.append('modelo', this.modelo);
+    }
+    return params;
+  }
+}
