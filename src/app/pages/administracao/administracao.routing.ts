5d1035bd7c4e39c89c965e053756c86029d61384
onseas <osmarneto750@gmail.com>
2022-09-07 16:58:16 -0300

    Adicionando ROLES restantes

--- src/app/pages/administracao/administracao.routing.ts
+++ src/app/pages/administracao/administracao.routing.ts
@@ -55,8 +55,16 @@ export const AdministracaoRoutes: Routes = [
           'ROLE_CLIENTE_VISUALIZAR',
           'ROLE_CLIENTE_STATUS',
 
-          // COLOCAR OS ROLES DAS ARMAS
+          'ROLE_GERENCIARARMA_VISUALIZAR',
+          'ROLE_GERENCIARARMA_PESQUISAR',
+          'ROLE_GERENCIARARMA_ALTERAR',
 
+          'ROLE_TRAFEGARARMAS_OPERACAO',
+
+          'ROLE_ENTRADAARMA_INCLUIR',
+
+          'ROLE_SAIDAARMA_VENDIDASEMESTOQUE',
+          'ROLE_SAIDAARMA_SAIDA',
 
         ]
       }
